f2a9a1f35bddaaa41ed6fb566d7d3e258aa39626
Gb3ar <benmct06@gmail.com>
2025-05-02 10:04:49 -0500

    No change intitial push

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#Prompting People 
+#Manual python3.13 -m pydoc input 
+
+age = input('How old are you? ')
+height = input('How tall are you? ')
+weight = input('How much do you weigh? ')
+
+print (f"So, you are {age} old, {height} tall and weigh {weight} you fatty!!!")
---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here are some new stange stuff, remember type it exacly!!!!
+
+days = "Mon-Tue-Wed-Thur-Fri-Sat-Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print ("""
+       There's something going on here.
+       With the three double quote.
+       We'll be able to type as much as we would like.
+       Even 4 lines if we want, or 5, or 6.
+       """)
