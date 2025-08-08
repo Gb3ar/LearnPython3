2330e2383fc03cf1bf7461b744439fff5abc04b9
Gb3ar <benmct06@gmail.com>
2025-08-08 15:38:17 -0500

    messed up everthing

--- ex12.py
+++
@@ -1,8 +0,0 @@
-#Prompting People 
-#Manual python3.13 -m pydoc input 
-
-age = input('How old are you? ')
-height = input('How tall are you? ')
-weight = input('How much do you weigh? ')
-
-print (f"So, you are {age} old, {height} tall and weigh {weight} you fatty!!!")
